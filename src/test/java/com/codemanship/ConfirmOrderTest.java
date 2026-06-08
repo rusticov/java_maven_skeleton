df746b6c8ba984c626f52dff8ddf9ae905b305ff
Rusticov <knightdestruction@pm.me>
2026-06-08 23:13:45 +0100

    refactor(test): Extract common setup of inventory and order in ConfirmOrderTest

--- src/test/java/com/codemanship/ConfirmOrderTest.java
+++ src/test/java/com/codemanship/ConfirmOrderTest.java
@@ -6,18 +6,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConfirmOrderTest {
 
+    private final Inventory inventory = new Inventory(
+        new Product(new Product.Id(327), "Ibanez Tube Screamer", 7, 2),
+        new Product(new Product.Id(811), "Fender Deluxe Reverb", 2, 1)
+    );
+
+    private final Order order = new Order(
+        inventory,
+        new Order.Entry(new Product.Id(327), 2),
+        new Order.Entry(new Product.Id(811), 1)
+    );
+
     @Test
     void openOrderWhenConfirmedThenOrderStatusIsConfirmed() {
-        Inventory inventory = new Inventory(
-            new Product(new Product.Id(327), "Ibanez Tube Screamer", 7, 2),
-            new Product(new Product.Id(811), "Fender Deluxe Reverb", 2, 1)
-        );
-
-        Order order = new Order(
-            inventory,
-            new Order.Entry(new Product.Id(327), 2),
-            new Order.Entry(new Product.Id(811), 1)
-        );
         order.confirm();
 
         assertEquals(Order.Status.Confirmed, order.getStatus());
@@ -25,16 +26,6 @@ public class ConfirmOrderTest {
 
     @Test
     void openOrderWhenConfirmedThenOrderItemsAreReleasedFromOnHold() {
-        Inventory inventory = new Inventory(
-            new Product(new Product.Id(327), "Ibanez Tube Screamer", 7, 2),
-            new Product(new Product.Id(811), "Fender Deluxe Reverb", 2, 1)
-        );
-
-        Order order = new Order(
-            inventory,
-            new Order.Entry(new Product.Id(327), 2),
-            new Order.Entry(new Product.Id(811), 1)
-        );
         order.confirm();
 
         assertEquals(0, inventory.getProduct(new Product.Id(327)).onHold());
