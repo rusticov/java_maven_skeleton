30c34edecd44a415c24ca2d0b5e72e3a1ae274eb
Rusticov <knightdestruction@pm.me>
2026-06-11 00:41:38 +0100

    Country now has a Region.  The UK is deemed to be cheaper because it is local to the merchant.

--- src/main/java/com/codemanship/Country.java
+++ src/main/java/com/codemanship/Country.java
@@ -1,7 +1,13 @@
 package com.codemanship;
 
 public enum Country {
-    UNITED_KINGDOM,
-    FRANCE,
-    GERMANY
+    UNITED_KINGDOM(Region.LOCAL),
+    FRANCE(Region.EU),
+    GERMANY(Region.EU);
+
+    public final Region region;
+
+    Country(Region region) {
+        this.region = region;
+    }
 }
